88cf5bf5aed4471b63bee4a2f4d9dc1468e85367
teddy cubaka <teddycubaka96@gmail.com>
2022-12-02 19:53:23 +0100

    The sencond answer

--- aoc.js
+++ aoc.js
@@ -1,9 +1,13 @@
 const fs = require("fs");
 const path = "input.txt";
 
+// first challenge
+
 const me = ["X", "Y", "Z"];
 let result = [];
+let result2 = [];
 let count = 0;
+let count2 = 0;
 const getPoint = (opp, str) => {
 	if (str === "X") {
 		if (opp === "A") return 3;
@@ -20,6 +24,22 @@ const getPoint = (opp, str) => {
 	}
 };
 
+const getMyPoint = (opp, str) => {
+	if (str === "X") {
+		if (opp === "A") return 3;
+		if (opp === "B") return 1;
+		if (opp === "C") return 2;
+	} else if (str === "Y") {
+		if (opp === "A") return 4;
+		if (opp === "B") return 5;
+		if (opp === "C") return 6;
+	} else if (str === "Z") {
+		if (opp === "A") return 8;
+		if (opp === "B") return 9;
+		if (opp === "C") return 7;
+	}
+};
+
 fs.readFile(path, (err, data) => {
 	let games = [];
 	if (err) throw err;
@@ -30,6 +50,14 @@ fs.readFile(path, (err, data) => {
 				1 +
 				getPoint(val.split(" ")[0], val.split(" ")[1])
 		);
+		result2.push(getMyPoint(val.split(" ")[0], val.split(" ")[1]));
 	});
+
+	//the unser for the first question
 	result.map((num) => (count += num));
+	console.log(count);
+
+	//the unser for the second question
+	result2.map((num) => (count2 += num));
+	console.log(count2);
 });
