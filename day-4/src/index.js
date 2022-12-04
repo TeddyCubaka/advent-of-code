bef4debac971faa8a532263ea786cf23db8f07a6
teddy cubaka <teddycubaka96@gmail.com>
2022-12-04 14:26:01 +0100

    day-4, first question

---
+++ day-4/src/index.js
@@ -0,0 +1,34 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var count = 0;
+function comparator(arr1, arr2) {
+    if (arr1[0] <= arr2[0] && arr1[arr1.length - 1] >= arr2[arr2.length - 1]) {
+        count += 1;
+    }
+    else if (arr1[0] >= arr2[0] &&
+        arr1[arr1.length - 1] <= arr2[arr2.length - 1]) {
+        count += 1;
+    }
+    return;
+}
+fs.readFile("input.txt", function (err, data) {
+    if (err)
+        throw err;
+    var res = data.toString().split("\n");
+    var split = res.map(function (data) {
+        return data.split(",");
+    });
+    var compare = split.map(function (data) {
+        var i1 = data[0].split("-");
+        var i2 = data[1].split("-");
+        var num1 = i1.map(function (str) {
+            return parseInt(str, 10);
+        });
+        var num2 = i2.map(function (str) {
+            return parseInt(str, 10);
+        });
+        comparator(num1, num2);
+    });
+    console.log("count :", count);
+});
---
+++ day-4/src/index.ts
@@ -0,0 +1,34 @@
+import * as fs from "fs";
+
+let count: number = 0;
+function comparator(arr1: number[], arr2: number[]) {
+	if (arr1[0] <= arr2[0] && arr1[arr1.length - 1] >= arr2[arr2.length - 1]) {
+		count += 1;
+	} else if (
+		arr1[0] >= arr2[0] &&
+		arr1[arr1.length - 1] <= arr2[arr2.length - 1]
+	) {
+		count += 1;
+	}
+	return;
+}
+
+fs.readFile("input.txt", (err, data) => {
+	if (err) throw err;
+	let res = data.toString().split("\n");
+	let split = res.map((data) => {
+		return data.split(",");
+	});
+	let compare = split.map((data) => {
+		let i1: string[] = data[0].split("-");
+		let i2: string[] = data[1].split("-");
+		let num1: number[] = i1.map((str) => {
+			return parseInt(str, 10);
+		});
+		let num2: number[] = i2.map((str) => {
+			return parseInt(str, 10);
+		});
+		comparator(num1, num2);
+	});
+	console.log("count :", count);
+});
