0d65c685b3ec6988b76dd430e61941bb7b31ca48
teddy cubaka <teddycubaka96@gmail.com>
2022-12-05 22:43:50 +0100

    day-5, add readme file

--- day-5/src/index.js
+++ day-5/src/index.js
@@ -9,12 +9,10 @@ var topCrate2 = "";
 function CrateMover9001(arr) {
     arr.map(function (data) {
         var move = data.split(" ");
-        var s1 = secStacks[parseInt(move[3]) - 1].suite;
-        var s2 = secStacks[parseInt(move[5]) - 1].suite;
         for (var i = parseInt(move[1]) - 1; i >= 0; i--) {
-            s2.unshift(s1[i]);
+            secStacks[parseInt(move[5]) - 1].suite.unshift(secStacks[parseInt(move[3]) - 1].suite[i]);
         }
-        s1.splice(0, parseInt(move[1]));
+        secStacks[parseInt(move[3]) - 1].suite.splice(0, parseInt(move[1]));
     });
     secStacks.map(function (data) {
         topCrate2 += data.suite[0];
@@ -31,9 +29,7 @@ fs.readFile("inputs.txt", function (err, data) {
         if (data.charCodeAt(0) >= 65 && data.charCodeAt(0) <= 90)
             position.push(index);
     });
-    position
-        .sort(function (a, b) { return a - b; })
-        .map(function (index) {
+    position.map(function (index) {
         stacks.push({
             index: index,
             suite: []
@@ -43,7 +39,7 @@ fs.readFile("inputs.txt", function (err, data) {
             suite: []
         });
     });
-    res.map(function (data, id) {
+    res.map(function (data) {
         data.split("").map(function (str, index) {
             if (position.indexOf(index) > -1 && str !== " ") {
                 stacks[position.indexOf(index)].suite.push(str);
@@ -54,25 +50,20 @@ fs.readFile("inputs.txt", function (err, data) {
     fs.readFile("range.txt", function (err, res) {
         if (err)
             throw err;
+        CrateMover9001(res.toString().split("\n"));
         res
             .toString()
             .split("\n")
             .map(function (data) {
             var move = data.split(" ");
-            var i1 = parseInt(move[1]);
-            var i2 = parseInt(move[3]) - 1;
-            var i3 = parseInt(move[5]) - 1;
-            var s1 = stacks[i2].suite;
-            var s2 = stacks[i3].suite;
-            for (var i = 0; i < i1; i++) {
-                s2.unshift(s1[i]);
+            for (var i = 0; i < parseInt(move[1]); i++) {
+                stacks[parseInt(move[5]) - 1].suite.unshift(stacks[parseInt(move[3]) - 1].suite[i]);
             }
-            s1.splice(0, parseInt(move[1]));
+            stacks[parseInt(move[3]) - 1].suite.splice(0, parseInt(move[1]));
         });
         stacks.map(function (data) {
             topCrate += data.suite[0];
         });
-        CrateMover9001(res.toString().split("\n"));
         console.log("first string :", topCrate);
     });
 });
--- day-5/src/index.ts
+++ day-5/src/index.ts
@@ -15,13 +15,13 @@ let topCrate2: string = "";
 function CrateMover9001(arr: string[]): string {
 	arr.map((data) => {
 		const move = data.split(" ");
-		const s1 = secStacks[parseInt(move[3]) - 1].suite;
-		const s2 = secStacks[parseInt(move[5]) - 1].suite;
 
 		for (let i = parseInt(move[1]) - 1; i >= 0; i--) {
-			s2.unshift(s1[i]);
+			secStacks[parseInt(move[5]) - 1].suite.unshift(
+				secStacks[parseInt(move[3]) - 1].suite[i]
+			);
 		}
-		s1.splice(0, parseInt(move[1]));
+		secStacks[parseInt(move[3]) - 1].suite.splice(0, parseInt(move[1]));
 	});
 
 	secStacks.map((data) => {
@@ -42,20 +42,18 @@ fs.readFile("inputs.txt", (err, data) => {
 			position.push(index);
 	});
 
-	position
-		.sort((a, b) => a - b)
-		.map((index) => {
-			stacks.push({
-				index: index,
-				suite: [],
-			});
-			secStacks.push({
-				index: index,
-				suite: [],
-			});
+	position.map((index) => {
+		stacks.push({
+			index: index,
+			suite: [],
 		});
+		secStacks.push({
+			index: index,
+			suite: [],
+		});
+	});
 
-	res.map((data, id) => {
+	res.map((data) => {
 		data.split("").map((str, index) => {
 			if (position.indexOf(index) > -1 && str !== " ") {
 				stacks[position.indexOf(index)].suite.push(str);
@@ -66,26 +64,22 @@ fs.readFile("inputs.txt", (err, data) => {
 
 	fs.readFile("range.txt", (err, res) => {
 		if (err) throw err;
+		CrateMover9001(res.toString().split("\n"));
 		res
 			.toString()
 			.split("\n")
 			.map((data) => {
 				const move = data.split(" ");
-				const i1 = parseInt(move[1]);
-				const i2 = parseInt(move[3]) - 1;
-				const i3 = parseInt(move[5]) - 1;
-				const s1 = stacks[i2].suite;
-				const s2 = stacks[i3].suite;
-
-				for (let i = 0; i < i1; i++) {
-					s2.unshift(s1[i]);
+				for (let i = 0; i < parseInt(move[1]); i++) {
+					stacks[parseInt(move[5]) - 1].suite.unshift(
+						stacks[parseInt(move[3]) - 1].suite[i]
+					);
 				}
-				s1.splice(0, parseInt(move[1]));
+				stacks[parseInt(move[3]) - 1].suite.splice(0, parseInt(move[1]));
 			});
 		stacks.map((data) => {
 			topCrate += data.suite[0];
 		});
-		CrateMover9001(res.toString().split("\n"));
 		console.log("first string :", topCrate);
 	});
 });
