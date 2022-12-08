9fc4c7363e13cb30de03bf7887247675809c8f4c
teddy cubaka <teddycubaka96@gmail.com>
2022-12-08 16:45:42 +0100

    refactor : day-4

--- day-4/src/index.js
+++ day-4/src/index.js
@@ -1,45 +1,47 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-var count = 0;
-var sum = 0;
+var count_conteners = 0;
+var count_count_overlappers = 0;
 function comparator(arr1, arr2) {
     if (arr1[0] <= arr2[0] && arr1[arr1.length - 1] >= arr2[arr2.length - 1]) {
-        count += 1;
+        count_conteners += 1;
     }
     else if (arr1[0] >= arr2[0] &&
         arr1[arr1.length - 1] <= arr2[arr2.length - 1]) {
-        count += 1;
+        count_conteners += 1;
     }
     return;
 }
 function overlap(arr1, arr2) {
     if (arr1[0] <= arr2[1] && arr1[1] >= arr2[0]) {
-        sum += 1;
+        count_count_overlappers += 1;
     }
     else if (arr1[0] >= arr2[1] && arr1[1] <= arr2[0]) {
-        sum += 1;
+        count_count_overlappers += 1;
     }
 }
 fs.readFile("input.txt", function (err, data) {
     if (err)
         throw err;
-    var res = data.toString().split("\n");
-    var split = res.map(function (data) {
+    var elves_places = data
+        .toString()
+        .split("\n")
+        .map(function (data) {
         return data.split(",");
     });
-    var compare = split.map(function (data) {
-        var i1 = data[0].split("-");
-        var i2 = data[1].split("-");
-        var num1 = i1.map(function (str) {
+    var compare = elves_places.map(function (data) {
+        var first_places = data[0].split("-");
+        var second_places = data[1].split("-");
+        var first_elf_s_places = first_places.map(function (str) {
             return parseInt(str, 10);
         });
-        var num2 = i2.map(function (str) {
+        var second_elf_s_places = second_places.map(function (str) {
             return parseInt(str, 10);
         });
-        comparator(num1, num2);
-        overlap(num1, num2);
+        comparator(first_elf_s_places, second_elf_s_places);
+        overlap(first_elf_s_places, second_elf_s_places);
     });
-    console.log("count :", count);
-    console.log("sum :", sum);
+    console.log("first output :", count_conteners);
+    console.log("second output :", count_count_overlappers);
 });
--- day-4/src/index.ts
+++ day-4/src/index.ts
@@ -1,46 +1,48 @@
 import * as fs from "fs";
 
-let count: number = 0;
-let sum: number = 0;
+let count_conteners: number = 0;
+let count_count_overlappers: number = 0;
 
 function comparator(arr1: number[], arr2: number[]) {
 	if (arr1[0] <= arr2[0] && arr1[arr1.length - 1] >= arr2[arr2.length - 1]) {
-		count += 1;
+		count_conteners += 1;
 	} else if (
 		arr1[0] >= arr2[0] &&
 		arr1[arr1.length - 1] <= arr2[arr2.length - 1]
 	) {
-		count += 1;
+		count_conteners += 1;
 	}
 	return;
 }
 
 function overlap(arr1: number[], arr2: number[]) {
 	if (arr1[0] <= arr2[1] && arr1[1] >= arr2[0]) {
-		sum += 1;
+		count_count_overlappers += 1;
 	} else if (arr1[0] >= arr2[1] && arr1[1] <= arr2[0]) {
-		sum += 1;
+		count_count_overlappers += 1;
 	}
 }
 
 fs.readFile("input.txt", (err, data) => {
 	if (err) throw err;
-	let res = data.toString().split("\n");
-	let split = res.map((data) => {
-		return data.split(",");
-	});
-	let compare = split.map((data) => {
-		let i1: string[] = data[0].split("-");
-		let i2: string[] = data[1].split("-");
-		let num1: number[] = i1.map((str) => {
+	let elves_places: string[][] = data
+		.toString()
+		.split("\n")
+		.map((data) => {
+			return data.split(",");
+		});
+	let compare = elves_places.map((data) => {
+		let first_places: string[] = data[0].split("-");
+		let second_places: string[] = data[1].split("-");
+		let first_elf_s_places: number[] = first_places.map((str) => {
 			return parseInt(str, 10);
 		});
-		let num2: number[] = i2.map((str) => {
+		let second_elf_s_places: number[] = second_places.map((str) => {
 			return parseInt(str, 10);
 		});
-		comparator(num1, num2);
-		overlap(num1, num2);
+		comparator(first_elf_s_places, second_elf_s_places);
+		overlap(first_elf_s_places, second_elf_s_places);
 	});
-	console.log("count :", count);
-	console.log("sum :", sum);
+	console.log("first output :", count_conteners);
+	console.log("second output :", count_count_overlappers);
 });
